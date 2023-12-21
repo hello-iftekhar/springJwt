d1395235ad7ab5c6506ed6514ef612e457c572ef
Iftekhar Hossain <helloiftekhar18@gmail.com>
2023-12-21 15:36:26 +0600

    initial commit

---
+++ src/main/java/com/helloiftekhar/springJwt/SpringJwtApplication.java
@@ -0,0 +1,13 @@
+package com.helloiftekhar.springJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloiftekhar/springJwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloiftekhar.springJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
